60a3fbfae3bb7eb1ea3c65aab8abe8933f49d975
gabe <gabe@delgadobunch.com>
2022-03-25 18:19:49 -0400

    added fontawesome script, tailwind scripts, gutted template app, added unfinished home route, renamed react files to jsx for vscode

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <div className=" w-[99vw] h-[99vh] flex justify-center items-center">
+                <p>hi threre</p>
+                <i className="fas fa-bars"></i>
+            </div>
+        </div>
+    )
+}
+
+export default Home
