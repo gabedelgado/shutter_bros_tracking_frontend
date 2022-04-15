31d054cfc3e8b92bf32003ecaf0816e5f2a85e3a
gabe <gabe@delgadobunch.com>
2022-04-15 14:57:45 -0400

    removed the width from Input component, was never used

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,9 @@
 
-const Input = ({placeholder, text, setText, width}) => {
+const Input = ({placeholder, text, setText}) => {
     
     return (
         <div className={`mb-3 pt-0 `}>
-            <input value={text} onChange={(e) => setText(e.target.value)} type="text" placeholder={placeholder} className={`text-md lg:text-xl px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white rounded border border-slate-300 outline-none focus:outline-none focus:ring ${width ? `w-[${width}vw]` : "w-full"}`}/>
+            <input value={text} onChange={(e) => setText(e.target.value)} type="text" placeholder={placeholder} className={`text-md lg:text-xl px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white rounded border border-slate-300 outline-none focus:outline-none focus:ring w-full`}/>
         </div>
     )
 }
