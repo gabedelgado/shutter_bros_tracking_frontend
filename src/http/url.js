80f79889500619af6b13d30b1bc808fa786f6157
gabe <gabe@delgadobunch.com>
2022-12-07 15:08:50 -0500

    updated backend url

--- src/http/url.js
+++ src/http/url.js
@@ -1,2 +1,3 @@
-export const url = "https://shutter-brothers-tracking.herokuapp.com/api";
+//export const url = "https://shutter-brothers-tracking.herokuapp.com/api";
 // export const url = "http://localhost:5000/api";
+export const url = "https://shutter-brothers-tracking-api.onrender.com/api";
